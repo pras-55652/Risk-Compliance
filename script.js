1eaadc0854a2ae5f3f3f0968b99fbad11f8a9bde
pras-55652 <mprahastika@gmail.com>
2026-09-14 13:24:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1136,6 +1136,8 @@ function renderTables() {
               </div>
             `;
           }
+
+          const formattedRegDate = p.regDate
           
           } else {
             actionHtml = `
